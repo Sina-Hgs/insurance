5ccf6bc96af2bce47e90ed048471bd182e6b31c8
Sina Haghshenas <s.haghshenas@podro.com>
2025-08-06 00:58:31 +0330

    feat/modal component

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 export { Button } from "./Button";
 export { Input } from "./Input";
 export { Dropdown } from "./Dropdown";
+export { Modal } from "./Modal";
 
 export { Navbar } from "./Navbar";
 export { MainLayout } from "./MainLayout";
